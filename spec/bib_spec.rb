4dea51077a631247e28504b17e19340f49f53555
alu0100825145 <alu0100825145@ull.edu.es>
2015-11-05 15:42:53 +0000

    Falta inicializar la editorial

--- spec/bib_spec.rb
+++ spec/bib_spec.rb
@@ -15,13 +15,19 @@ describe Biblio do
     
     describe "Titulo" do
         it "Debe existir un título" do
-            @libro.titulo.should eq("Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide")    
+            @libro.titulo.should eq("Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide", "Pragmatic Bookshelf;")    
         end 
     end
     
     describe "Serie" do
         it "Debe existir o no una serie" do
             @libro.serie.should eq("(The Facets of Ruby)")    
+        end 
+    end
+    
+    describe "Editorial" do
+        it "Debe existir una editorial" do
+            @libro.editorial.should eq("Pragmatic Bookshelf;")    
         end 
     end
     
