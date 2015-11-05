a7beed19ed8e96491c8c68ecf82fce27a7bf4731
alu0100826999 <alu0100826999@ull.edu.es>
2015-11-05 15:53:22 +0000

    Fecha de publicacion inicializada

--- lib/Bib.rb
+++ lib/Bib.rb
@@ -1,13 +1,14 @@
 class Biblio
     
-    attr_reader :autores, :titulo, :serie, :editorial, :numedicion
+    attr_reader :autores, :titulo, :serie, :editorial, :numedicion, :fecha
         
-    def initialize(autores, titulo, serie = nil, editorial, numedicion)
+    def initialize(autores, titulo, serie = nil, editorial, numedicion, fecha)
         @autores = autores
         @titulo = titulo
         @serie = serie
         @editorial = editorial
         @numedicion = numedicion
+        @fecha = fecha
     end
     
 end
