1fd67d19bd46c440288e32d8eb1ed04b65a9b449
alu0100825145 <alu0100825145@ull.edu.es>
2015-11-05 15:34:12 +0000

    Falta inicializar el titulo

--- spec/bib_spec.rb
+++ spec/bib_spec.rb
@@ -4,12 +4,18 @@ require "Bib"
 describe Biblio do
 
     before :each do
-        @libro = Biblio.new("Dave Thomas")
+        @libro = Biblio.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide")
     end
     
     describe "Autor" do
-        it "existe un método para obtener el listado de autores" do
-            @libro.autores.should eq("Dave Thomas")    
+        it "Debe existir uno o más autores" do
+            @libro.autores.should eq(["Dave Thomas", "Andy Hunt", "Chad Fowler"])    
+        end 
+    end
+    
+    describe "Titulo" do
+        it "Debe existir un título" do
+            @libro.titulo.should eq("Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide")    
         end 
     end
     
