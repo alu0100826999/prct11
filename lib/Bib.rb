cb0796f7eacc856263a4711619561460c6f77747
alu0100826999 <alu0100826999@ull.edu.es>
2015-11-05 15:35:42 +0000

    Titulo inicializado

--- lib/Bib.rb
+++ lib/Bib.rb
@@ -1,9 +1,10 @@
 class Biblio
     
-    attr_reader :autores
+    attr_reader :autores, :titulo
         
-    def initialize(autores)
-        @autores = autores 
+    def initialize(autores, titulo)
+        @autores = autores
+        @titulo = titulo
     end
     
 end
