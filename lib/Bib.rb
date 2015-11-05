0d6ef1296d3803daddb8cc87edc4bf5328e024a4
alu0100826999 <alu0100826999@ull.edu.es>
2015-11-05 15:40:01 +0000

    Serie inicializada

--- lib/Bib.rb
+++ lib/Bib.rb
@@ -1,10 +1,11 @@
 class Biblio
     
-    attr_reader :autores, :titulo
+    attr_reader :autores, :titulo, :serie
         
-    def initialize(autores, titulo)
+    def initialize(autores, titulo, serie = nil)
         @autores = autores
         @titulo = titulo
+        @serie = serie
     end
     
 end
