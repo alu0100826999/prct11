f9aec3bd2268a184bbea6f46116114de9f278e4f
alu0100826999 <alu0100826999@ull.edu.es>
2015-11-05 15:49:34 +0000

    Inicializado numero de edicion

--- lib/Bib.rb
+++ lib/Bib.rb
@@ -1,12 +1,13 @@
 class Biblio
     
-    attr_reader :autores, :titulo, :serie, :editorial
+    attr_reader :autores, :titulo, :serie, :editorial, :numedicion
         
-    def initialize(autores, titulo, serie = nil, editorial)
+    def initialize(autores, titulo, serie = nil, editorial, numedicion)
         @autores = autores
         @titulo = titulo
         @serie = serie
         @editorial = editorial
+        @numedicion = numedicion
     end
     
 end
