8436fae4dd4e8a5613c214eae5716f80f38e06ed
alu0100826999 <alu0100826999@ull.edu.es>
2015-11-12 15:05:57 +0000

    Inicializado nodo y creada estructura de nodo

--- lib/Bib/Lista.rb
+++ lib/Bib/Lista.rb
@@ -1,3 +1,8 @@
+Nodo = Struct.new(:value, :next)
+
 class Lista
-    
+    attr_reader :inicio
+    def initialize(nodo)
+       @inicio = Nodo.new(nil) 
+    end
 end
--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -3,7 +3,7 @@ require "Bib"
 
 describe Lista do
     
-    nodo1 = Nodo.new(2,nill)
-    nodo2 = Nodo.new(3,nill)
+    @nodo1 = Nodo.new(2,nil)
+    @nodo2 = Nodo.new(3,nil)
     
 end
