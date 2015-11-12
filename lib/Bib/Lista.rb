6d6941023d1d6baf2073ffefa795167ac48a54c9
alu0100826999 <alu0100826999@ull.edu.es>
2015-11-12 16:54:34 +0000

    Olvide el commit del error al crear la prueba para Extraer el primer elemento de la lista

--- lib/Bib/Lista.rb
+++ lib/Bib/Lista.rb
@@ -24,7 +24,7 @@ class Lista
             aux = @inicio
             @inicio = @inicio.next #para imprimir el segundo que metemos. 
             #Nos piden el primer nodo asi que pasamos el nodo cero y extraemos el siguiente que es el primero.
-            #Si uitamos next nos extrae el nodo 0
+            #Si uitamos next nos extrae el nodo 0.
             return aux
         else
             return nil
