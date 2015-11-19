e66ebc9a28846d38cbd079ed5d608da0dfd0df1b
alu0100826999 <alu0100826999@ull.edu.es>
2015-11-19 16:44:01 +0000

    Codigo terminado. Nos queda utilizar el guard

--- lib/Bib/Bib.rb
+++ lib/Bib/Bib.rb
@@ -1,10 +1,26 @@
 class Biblio
     
-    attr_reader :autores, :titulo, :serie, :editorial, :numedicion, :fecha, :isbn
+    attr_reader :autores, :titulo
         
-    def initialize(autores, titulo, serie = nil, editorial, numedicion, fecha, isbn)
+    def initialize(autores, titulo)
         @autores = autores
         @titulo = titulo
+    end
+    
+    def to_s()
+        "#{@autores[0]}, #{@autores[1]}, #{@autores[2]} \n #{@titulo}"
+
+    end
+    
+end
+
+
+class Libro < Biblio
+    
+    attr_reader :serie, :editorial, :numedicion, :fecha, :isbn
+    
+    def initialize(autores, titulo, serie = nil, editorial, numedicion, fecha, isbn)
+        super(autores, titulo)
         @serie = serie
         @editorial = editorial
         @numedicion = numedicion
@@ -12,48 +28,42 @@ class Biblio
         @isbn = isbn
     end
     
-    def to_s()
-       "#{@autores[0]}, #{@autores[1]}, #{@autores[2]} \n #{@titulo} \n #{@serie} \n #{@editorial} \n #{@numedicion} \n #{@fecha} \n #{@isbn}"
-    end
-    
-end
-
-class Libro < Biblio
-    
-    attr_reader :autores, :titulo, :serie, :editorial, :numedicion, :fecha, :isbn
-        
-    def initialize(autores, titulo, serie = nil, editorial, numedicion, fecha, isbn)
-        super(autores, titulo, serie, editorial, numedicion, fecha, isbn)
-    end
-    
 end
 
 class Revista < Biblio
     
-    attr_reader :autores, :titulo, :serie, :editorial, :numedicion, :fecha, :isbn
+    attr_reader :serie, :editorial, :issn
         
-    def initialize(autores, titulo, serie = nil, editorial, numedicion, fecha, isbn)
-        super(autores, titulo, serie, editorial, numedicion, fecha, isbn)
+    def initialize(autores, titulo, serie = nil, editorial, issn)
+        super(autores, titulo)
+        @serie = serie
+        @issn = issn
+        @editorial = editorial
     end
     
 end
+
 
 class Periodico < Biblio
     
-    attr_reader :autores, :titulo, :serie, :editorial, :numedicion, :fecha, :isbn
+    attr_reader :editor, :periodista
         
-    def initialize(autores, titulo, serie = nil, editorial, numedicion, fecha, isbn)
-        super(autores, titulo, serie, editorial, numedicion, fecha, isbn)
+    def initialize(autores, titulo, editor, periodista)
+        super(autores, titulo)
+        @editor = editor
+        @periodista = periodista
     end
     
 end
 
+
 class Electronico < Biblio
     
-    attr_reader :autores, :titulo, :serie, :editorial, :numedicion, :fecha, :isbn
+    attr_reader :url
         
-    def initialize(autores, titulo, serie = nil, editorial, numedicion, fecha, isbn)
-        super(autores, titulo, serie, editorial, numedicion, fecha, isbn)
+    def initialize(autores, titulo, url)
+        super(autores, titulo)
+        @url = url
     end
     
 end
--- spec/bib_spec.rb
+++ spec/bib_spec.rb
@@ -4,11 +4,11 @@ require "Bib"
 describe Biblio do
 
     before :each do
-        @libro1 = Biblio.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide", "(The Facets of Ruby)", "Pragmatic Bookshelf", "4 edition", "(July 7, 2013)", ["ISBN-13: 978-1937785499","ISBN-10: 1937785491"])
-        @libro2 = Biblio.new("Scott Chacon", "Pro Git 2009th Edition" "Pro", "Apress", "2009 edition", "August 27, 2009", ["ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
-        @libro3 = Biblio.new(["David Flanagan, Yukihiro Matsumoto"], "The Ruby Programming Language", "", "O’Reilly Media", "1 edition", "February 4, 2008", ["ISBN-10: 0596516177", "ISBN-13: 978-0596516178"])
-        @libro4 = Biblio.new(["David Chelimsky, Dave Astels, Bryan Helmkamp, Dan North, Zach Dennis, Aslak Hellesoy"], "The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends", "The Facets of Ruby", "Pragmatic Bookshelf", "1 edition", "December 25, 2010", ["ISBN-10: 1934356379", "ISBN-13: 978-1934356371"])
-        @libro5 = Biblio.new("Richard E. Silverman", "Git Pocket Guide", "", "O’Reilly Media", "1 edition", "August 2, 2013", ["ISBN-10: 1449325866", "ISBN-13: 978-1449325862"])
+        @libro1 = Libro.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide", "(The Facets of Ruby)", "Pragmatic Bookshelf", "4 edition", "(July 7, 2013)", ["ISBN-13: 978-1937785499","ISBN-10: 1937785491"])
+        @libro2 = Libro.new("Scott Chacon", "Pro Git 2009th Edition" "Pro", "Apress", "2009 edition", "August 27, 2009", ["ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
+        @libro3 = Libro.new(["David Flanagan, Yukihiro Matsumoto"], "The Ruby Programming Language", "", "O’Reilly Media", "1 edition", "February 4, 2008", ["ISBN-10: 0596516177", "ISBN-13: 978-0596516178"])
+        @libro4 = Libro.new(["David Chelimsky, Dave Astels, Bryan Helmkamp, Dan North, Zach Dennis, Aslak Hellesoy"], "The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends", "The Facets of Ruby", "Pragmatic Bookshelf", "1 edition", "December 25, 2010", ["ISBN-10: 1934356379", "ISBN-13: 978-1934356371"])
+        @libro5 = Libro.new("Richard E. Silverman", "Git Pocket Guide", "", "O’Reilly Media", "1 edition", "August 2, 2013", ["ISBN-10: 1449325866", "ISBN-13: 978-1449325862"])
         
 	    @lista= Lista.new()
         
@@ -18,9 +18,9 @@ describe Biblio do
         @nodo4 = Nodo.new(@libro4, nil)
         @nodo5 = Nodo.new(@libro5, nil)
         
-        @rev1 = Revista.new("Josefa", "Vogue", "", "O’Reilly Media", "1 edition", "February 4, 2008", "ISBN-10: 1449325866")
-        @per1 = Periodico.new("Scott Chacon", "Pro Git 2009th Edition" "Pro", "Apress", "2009 edition", "August 27, 2009", ["ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
-        @elec1 = Electronico.new("Yukihiro Matsumoto", "The Ruby Programming Language", "", "O’Reilly Media", "1 edition", "February 4, 2008", "ISBN-13: 978-0596516178")
+        @rev1 = Revista.new("Josefa", "Vogue", "", "1 edition", "ISNN-10: 1449325866")
+        @per1 = Periodico.new("Scott Chacon", "Hola", "David Flanagan", "Richard Silverman")
+        @elec1 = Electronico.new("Yukihiro Matsumoto", "The Ruby Programming Language", "https://www.google.es/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8")
         
         @nodo6 = Nodo2.new(@libro1, nil, nil)
 
