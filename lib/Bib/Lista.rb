8f6166d5c5cb989400a3b6da0b44ea3897dcb989
alu0100826999 <alu0100826999@ull.edu.es>
2015-12-17 16:43:07 +0000

    Añadido to_s de la clase lista que nos faltó de la práctica anterior

--- lib/Bib/Lista.rb
+++ lib/Bib/Lista.rb
@@ -69,12 +69,30 @@ class Listadoble
         end
         
         def each
-           nodo = @head
-           while (nodo != nil) 
+            nodo = @head
+            while (nodo != nil) 
                 yield nodo.value
                 nodo = nodo.next
-           end
-       end
-    
-    
+            end
+        end
+        
+        
+        def to_s()
+            
+            lista_ref = []
+            index= 0
+            
+            while(@inicio != nil)
+            
+                lista_ref1 = @inicio.value.to_s()
+                lista_ref[index]= lista_ref1
+                @inicio= @inicio.next
+                index += 1
+                
+            end
+            
+            lista_ref
+            
+        end
+       
 end
