bfab0e5b3af38246ec3f96d76e839a11b184e795
alu0100826999 <alu0100826999@ull.edu.es>
2015-12-17 17:50:33 +0000

    Bloque para Resvista creado con exito. Sin fallos

--- lib/Bib/Bib.rb
+++ lib/Bib/Bib.rb
@@ -67,3 +67,67 @@ class Libro < Biblio
     
 end
 
+class Revista < Biblio
+    
+    attr_reader :autor, :serie, :editorial, :issn
+        
+    def initialize(titulo, &block)
+        @autor = []
+        @titulo = titulo
+        @serie = []
+        @issn = []
+        @editorial = []
+        
+        if block_given?
+            if block.arity == 1
+                yield self
+            else
+                instance_eval &block
+            end
+        end
+    end
+    
+    def author(text, options = {})
+        author = text
+        author << "(#{options[:name]})" if options[:name]
+        autor << author
+    end
+    
+    def edition(text, options = {})
+        edition = text
+        edition << "(#{options[:numedition]})" if options[:numedition]
+        editorial << edition
+    end
+    
+    def b_issn(text, options = {})
+       b_issn = text
+       b_issn << "(#{options[:issn]})" if options[:issn]
+       issn << b_issn
+    end
+    
+end
+
+
+class Periodico < Biblio
+    
+    attr_reader :editor, :periodista
+        
+    def initialize(autores, titulo, editor, periodista)
+        super(autores, titulo)
+        @editor = editor
+        @periodista = periodista
+    end
+    
+end
+
+
+class Electronico < Biblio
+    
+    attr_reader :url
+        
+    def initialize(autores, titulo, url)
+        super(autores, titulo)
+        @url = url
+    end
+    
+end
--- spec/bib_spec.rb
+++ spec/bib_spec.rb
@@ -5,17 +5,28 @@ describe Biblio do
     
     context "Libros" do
 
-        libro11 = Libro.new("El principe de la niebla") do
-            author  :name => "Nombre"
+        libro1 = Libro.new("El principe de la niebla") do
+            author  :name => "Carlos Ruiz"
             edition :numedition => "20"
             date "15 nov 1993"
         end
         
         it "Creamos un DSL con el libro" do
-            expect(libro11.kind_of?Libro).to eq(true)
+            expect(libro1.kind_of?Libro).to eq(true)
         end
         
-        puts libro11
+    end
+    
+    context "Revista" do
+        revista1 = Revista.new("Revista Vogue") do
+            author :name => "Anna Wintour"
+            edition :numedition => "1000"
+            b_issn "123456789"
+        end
+        
+        it "Creamos un DSL con la revista" do
+            expect(revista1.kind_of?Revista).to eq(true)
+        end
         
     end
     
